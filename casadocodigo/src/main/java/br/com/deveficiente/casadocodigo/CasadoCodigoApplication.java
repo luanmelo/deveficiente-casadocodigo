c649ec050007357af6ad99c2645fa397b90c4e73
Luan Melo <luan@luanmelo.com.br>
2020-01-24 22:39:41 -0300

    Ajuste para obrigar o livro ter uma categoria

--- casadocodigo/src/main/java/br/com/deveficiente/casadocodigo/CasadoCodigoApplication.java
+++ casadocodigo/src/main/java/br/com/deveficiente/casadocodigo/CasadoCodigoApplication.java
@@ -2,10 +2,8 @@ package br.com.deveficiente.casadocodigo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan
 public class CasadoCodigoApplication {
 
 	public static void main(String[] args) {
